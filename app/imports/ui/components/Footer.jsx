123a9c6f93ff3b90a3fcfa48b75ce9a129ccd722
Nanami Kimoto <46579968+nakimoto@users.noreply.github.com>
2021-04-13 08:58:35 -1000

    email sign up addition with imports
    needs about page

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-
+import { Input } from 'semantic-ui-react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
     const divStyle = { paddingTop: '15px' };
     return (
       <footer>
-        <div style={divStyle} className="ui center aligned container">
+        <div style={divStyle} className="ui center aligned container" color='white'>
           <hr />
-              Contact Budget Munchies at nkimoto@hawaii.edu<br />
-              Sign up to recieve our email updates!<br />
-              <Input
-                action={ { color: 'black', content: 'Join' } }
-                placeholder="Enter Email Address"/>
-          <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Budget Munchies Home Page</a>
+          Contact Budget Munchies at nkimoto@hawaii.edu<br />
+          Sign up to get our email updates!<br />
+          <Input
+            action={ { color: 'black', content: 'Join' } }
+            placeholder="Enter Email Address"/>
+          <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
         </div>
       </footer>
     );
