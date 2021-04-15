e16d428068c1c8b7e0ed0563b2e9a8748454431c
kk <58120049+kierankjk@users.noreply.github.com>
2021-04-15 09:44:56 -1000

    first update of not found page

--- app/imports/ui/pages/NotFound.jsx
+++ app/imports/ui/pages/NotFound.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Container, Divider } from 'semantic-ui-react';
 
 /** Render a Not Found page if the user enters a URL that doesn't match any route. */
 class NotFound extends React.Component {
   render() {
     return (
-      <Header as="h2" textAlign="center">
-        <p>Page not found</p>
-      </Header>
+      <Container>
+        <Divider/>
+        <Header as="h2" textAlign="center" inverted>
+          <p> page not found :( </p>
+          <p>this page does not currently exist</p>
+        </Header>
+        <Divider/>
+      </Container>
     );
   }
 }
