2bc1bf23a8a1dff990e6cf00770c38b80e1f15c9
marissahalim <halimmq2@gmail.com>
2021-04-15 21:41:02 -1000

    updated colors in footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,19 +3,18 @@ import { Input } from 'semantic-ui-react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = { paddingTop: '15px', color: '#E3EEFA' };
     return (
       <footer>
         <div style={divStyle} className="ui center aligned container" color='white'>
-          <hr />
           Budget Munchies is a project by students, for students!<br/>
           Contact Budget Munchies at budgetmunchies@hawaii.edu<br />
           Sign up to get our email updates!<br />
+          <br/>
           <Input
             action={ { color: 'black', content: 'Join' } }
             placeholder="Enter Email Address"/>
-          <a href="https://budget-munchies.github.io">Budget Munchies Home Page</a>
-
+          <a href="https://budget-munchies.github.io">  Budget Munchies Home Page</a>
         </div>
       </footer>
     );
